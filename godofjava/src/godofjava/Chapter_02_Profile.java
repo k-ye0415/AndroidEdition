bfaa8fab750434ad45c1a6b51ca4e715de393449
YEJIN <ye_s_jin@naver.com>
2025-01-27 23:13:08 +0900

    20250127

---
+++ godofjava/src/godofjava/Chapter_02_Profile.java
@@ -0,0 +1,40 @@
+package godofjava;
+/*
+ * Chapter02. Hello God Of Java
+ * */
+public class Chapter_02_Profile {
+	public static void main(String[] args) {
+		String name="OOO";
+		int age=0;
+		System.out.println("My name is "+name);
+		System.out.println("My age is "+age);
+	}
+}
+
+
+/*
+ * 1. main() 메소드의 메소드 이름 앞에는 어떤 예약어들이 있는가
+ * 	public void
+ * 		-> public static void 가 정답
+ * 
+ * 2. main() 메소드의 매개 변수에는 어떤 값이 들어가나?
+ * 	없음
+ * 		-> String[] args 가 정답
+ * 
+ * 3. 클래스에 main() 함수가 없다면 java 명령어로 그 클래스를 수행할 수 있는가?
+ * 	수행 할 수 없음.
+ * 
+ * 4. System.out.println() 메소드의 용도는?
+ * 	log 용
+ * 		-> 줄바꿈 log 용
+ * 
+ * 5. System.out.print() 메소드는 System.out.println() 와 어떤 차이가 있는가
+ * 	줄바꿈
+ * 
+ * 6. // 는 어떤 기호인가
+ * 	주석용
+ * 
+ * 7. 메소드를 선언할 때 반드시 꼭 잇어야 하는 세가지
+ * 리턴타입, 메소드명, 메소드 내 
+ * */
+ 
