0c50d08336fd7e557fad51b9b339f306bf9d16a0
YEJIN <ye_s_jin@naver.com>
2025-01-31 00:26:41 +0900

    [Movie] Theme, Color 수정

---
+++ Android_compose/part_4_plus_ch_3_movie_renewal/app/src/main/java/com/jin/part_4_plus_ch_3_movie_renewal/ui/theme/Shapes.kt
@@ -0,0 +1,11 @@
+package com.jin.part_4_plus_ch_3_movie_renewal.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(8.dp),
+    large = RoundedCornerShape(12.dp)
+)
--- Android_compose/part_4_plus_ch_3_movie_renewal/app/src/main/java/com/jin/part_4_plus_ch_3_movie_renewal/ui/theme/Theme.kt
+++ Android_compose/part_4_plus_ch_3_movie_renewal/app/src/main/java/com/jin/part_4_plus_ch_3_movie_renewal/ui/theme/Theme.kt
@@ -1,58 +1,36 @@
 package com.jin.part_4_plus_ch_3_movie_renewal.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.staticCompositionLocalOf
+import com.jin.part_4_plus_ch_3_movie_renewal.ui.theme.color.ColorSet
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
-
+private val localColors = staticCompositionLocalOf {
+    ColorSet.Red.lightColors
+}
 @Composable
 fun Part_4_plus_ch_3_movie_renewalTheme(
+    myColors: ColorSet,
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    val colorScheme = if (darkTheme) {
+        myColors.darkColors
+    } else {
+        myColors.lightColors
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+    CompositionLocalProvider(localColors provides colorScheme) {
+        MaterialTheme(
+            colorScheme = colorScheme.material,
+            typography = Typography,
+            shapes = Shapes,
+            content = content
+        )
+    }
 }
